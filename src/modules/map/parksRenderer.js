b16cb0aa3e55dd897adcefe2ab020db140d0e31f
tkluczwajd <tkluczwajd@gmail.com>
2026-05-08 19:29:08 +0200

    Create parksRenderer.js

---
+++ src/modules/map/parksRenderer.js
@@ -0,0 +1,27 @@
+import { mapManager } from './mapManager.js';
+
+export function renderParksOnMap(places) {
+    // Używamy globalnego obiektu Leaflet
+    const L = window.L;
+
+    // Najpierw czyścimy starą warstwę parków, żeby pineski się nie dublowały
+    mapManager.clearLayer('parks');
+
+    places.forEach(place => {
+        const iconHtml = `<div style="background:${place.isDogPark ? '#4cd137' : '#00a8ff'}; width:30px; height:30px; border-radius:50%; display:flex; align-items:center; justify-content:center; color:white; border:2px solid white; box-shadow:var(--soft-shadow); font-size:16px;">${place.isDogPark ? '🐕' : '🌳'}</div>`;
+        
+        const icon = L.divIcon({
+            className: '',
+            html: iconHtml,
+            iconSize: [30, 30]
+        });
+        
+        const marker = L.marker([place.lat, place.lng], { icon });
+        
+        // Dodajemy dymek po kliknięciu
+        marker.bindPopup(`<b>${place.name}</b><br>${place.distance.toFixed(2)} km`);
+        
+        // Wrzucamy marker do dedykowanej warstwy 'parks'
+        mapManager.addMarkerToLayer('parks', marker);
+    });
+}
