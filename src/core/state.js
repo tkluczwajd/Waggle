ac87aba2722f1f67a256de727b8af37caf7fda0b
tkluczwajd <tkluczwajd@gmail.com>
2026-05-08 18:05:58 +0200

    Update state.js

--- src/core/state.js
+++ src/core/state.js
@@ -14,9 +14,14 @@ export const appState = {
         lng: null,
         following: true,
         watchId: null
-    }
+    },
+    profile: null, // Tu trzymamy dane usera (imię, awatar, tryb ducha)
+    isWalking: false,
+    map: null,
+    currentChatId: null
 };
 
+// Funkcja do bezpiecznej zmiany danych
 export function setState(path, value) {
     const keys = path.split('.');
     let current = appState;
@@ -25,3 +30,6 @@ export function setState(path, value) {
     }
     current[keys[0]] = value;
 }
+
+// Tymczasowy most dla Twoich starszych funkcji, by ich nie popsuć
+export const state = appState;
