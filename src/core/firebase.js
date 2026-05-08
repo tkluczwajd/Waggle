6b6a02f953d15a7a98068ef96c43e1f9e68f823e
tkluczwajd <tkluczwajd@gmail.com>
2026-05-08 18:02:12 +0200

    Update firebase.js

--- src/core/firebase.js
+++ src/core/firebase.js
@@ -1,3 +1,7 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
 const firebaseConfig = { 
     apiKey: "AIzaSyA7CSlyOLzbz2LpO0C-KqaZQ0U_OrNqBcg", 
     authDomain: "waggle-app-31ffa.firebaseapp.com", 
@@ -7,8 +11,10 @@ const firebaseConfig = {
     appId: "1:711707392068:web:b81c7e0714cfe24dd1e411" 
 };
 
-firebase.initializeApp(firebaseConfig);
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
 
-export const db = firebase.firestore();
 export const auth = firebase.auth();
-export const fb = firebase; // Potrzebne do FieldValue
+export const db = firebase.firestore();
+export const fb = firebase;
