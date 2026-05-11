a76c72bcaff0711e04de157043b91781bc7aac13
tkluczwajd <tkluczwajd@gmail.com>
2026-05-11 15:34:42 +0200

    Update profileService.js

--- src/services/profileService.js
+++ src/services/profileService.js
@@ -1,4 +1,4 @@
-import { db } from '../../core/firebase.js';
+import { db, fb } from '../core/firebase.js';
 
 export function saveProfileData(uid, data) {
     return db.collection("users").doc(uid).set(data, { merge: true });
