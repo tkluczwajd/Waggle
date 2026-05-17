fee6259dc6f249aaf74b33e1e1b89d24c3d1d227
tkluczwajd <tkluczwajd@gmail.com>
2026-05-17 19:39:25 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-// src/app.js - Punkt wejścia (Entry Point) aplikacji Waggle 🐾
+// src/app.js - Nowy, minimalistyczny punkt wejścia ekosystemu Waggle 🐾
 import { bootstrapApp } from './core/appBootstrap.js';
 
 bootstrapApp();
