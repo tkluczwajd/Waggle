3ad7184224d2e316ddd81912681b075e29d271a5
tkluczwajd <tkluczwajd@gmail.com>
2026-06-06 22:28:40 +0200

    Update wikiTrainingData.js

--- src/data/wikiTrainingData.js
+++ src/data/wikiTrainingData.js
@@ -1,46 +1,9 @@
-export const WIKI = {
-    trening: [
-        { 
-            id: "train_tree", 
-            title: "Metoda Drzewa (Luźna smycz)", 
-            keywords: ["smycz", "ciągnięcie", "spacer", "napięcie", "ciągnie"], 
-            desc: "Podstawowy trening spacerowy. Gdy tylko poczujesz, że smycz napina się choćby minimalnie, natychmiast zatrzymaj się i stój nieruchomo jak drzewo. Nie szarp psa, nie krzycz. Rusz przed siebie DOPIERO wtedy, gdy pies sam poluzuje smycz lub odwróci się i spojrzy na Ciebie. Uczy to psa, że tylko luźna smycz pozwala iść do przodu." 
-        },
-        { 
-            id: "train_asphalt", 
-            title: "Reguła 5 Sekund (Ochrona łap w lato)", 
-            keywords: ["lato", "asfalt", "upał", "poparzenie", "poduszki", "chodnik"], 
-            desc: "Zanim wyjdziesz z psem na spacer na rozgrzany chodnik w słoneczny dzień, przyłóż wewnętrzną część swojej dłoni płasko do asfaltu i przytrzymaj przez 5 sekund. Jeśli podłoże parzy Ciebie, oznacza to, że poparzy również delikatne opuszki Twojego psa. W upały spaceruj wyłącznie rano, wieczorem lub po trawiastych terenach leśnych." 
-        },
-        { 
-            id: "train_bingo", 
-            title: "Przywołanie Awaryjne (Słowo Życia)", 
-            keywords: ["ucieczka", "przywołanie", "bingo", "zagrożenie", "wróć", "wołąnie"], 
-            desc: "Wybierz jedno unikalne, dźwięczne słowo, którego NIGDY nie używasz na co dzień (np. 'BINGO!', 'BANAN!'). Przez kilka tygodni ćwicz w domu: wypowiedz słowo i natychmiast daj psu najlepszy smakołyk na świecie (np. kawałek pieczonego mięsa). To słowo ma kojarzyć się z absolutnym rajem. Używaj lo w terenie TYLKO wtedy, gdy pies np. biegnie w stronę ulicy. Nigdy nie używaj go na darmo." 
-        },
-        { 
-            id: "train_rule3", 
-            title: "Reguła 3 Sekund (Głaskanie obcych psów)", 
-            keywords: ["głaskanie", "dotyk", "obcy pies", "dzieci", "kontakt", "gryzie"], 
-            desc: "Zanim dotkniesz jakiegokolwiek psa, zapytaj właściciela o zgodę. Jeśli ją uzyskasz, kucnij bokiem do psa (nigdy nie podchodź od przodu i nie pochylaj się nad nim – to gest groźby). Wyciągnij delikatnie dłoń do powąchania. Pogłaskaj psa przez dokładnie 3 sekundy pod brodą lub po klatce piersiowej (NIGDY po czubku głowy!), po czym cofnij rękę. Jeśli pies odejdzie – zostaw go. Jeśli podejdzie bliżej i trąci Cię noskiem – oznacza to, że chce więcej." 
-        },
-        { 
-            id: "train_clicker", 
-            title: "Trening Klikerowy (Zaznaczanie zachowań)", 
-            keywords: ["kliker", "szkolenie", "nagroda", "smakołyk", "siad", "nauka"], 
-            desc: "Kliker to małe urządzenie wydające powtarzalny dźwięk 'klik'. Najpierw 'warunkujemy' kliker: klikasz i natychmiast dajesz smakołyk (powtórz 20 razy). Dźwięk staje się dla psa obietnicą nagrody. Następnie podczas nauki np. komendy 'siad', klikasz DOKŁADNIE w milisekundzie, gdy pupa psa dotyka ziemi, i dajesz nagrodę. Kliker pozwala precyzyjnie wskazać psu, za które zachowanie dostaje wypłatę." 
-        },
-        { 
-            id: "train_cage", 
-            title: "Klatka Kennelowa (Bezpieczny azyl)", 
-            keywords: ["klatka", "kennel", "lęk", "niszczenie", "samotność", "odpoczynek"], 
-            desc: "Klatka kennelowa nie jest więzieniem – to sztuczna jaskinia i azyl dla psa. Proces wprowadzania klatki musi być powolny: wrzucaj tam najlepsze gryzaki, podawaj miski z jedzeniem, nigdy nie zamykaj drzwiczek na siłę. Pies ma sam wybierać spanie w kennelówce. Prawidłowo wprowadzona klatka drastycznie obniża poziom stresu u psów z lękiem separacyjnym i zapobiega niszczeniu mieszkania." 
-        },
-        { 
-            id: "train_eye", 
-            title: "Nauka Skupienia (Kontakt Wzrokowy)", 
-            keywords: ["skupienie", "uwaga", "patrz", "kontakt", "smycz", "bodźce"], 
-            desc: "Podstawa pracy z psem reaktywnym. Weź smakołyk do dłoni, zamknij ją w pięść i wyciągnij w bok. Pies będzie lizał i drapał rękę. Stój nieruchomo. W momencie, gdy pies sfrustrowany brakiem dostępu do jedzenia odwróci wzrok od dłoni i spojrzy Ci prosto w oczy – powiedz 'TAK' i daj mu nagrodę. Uczy to psa, że kluczem do wszystkiego jest kontakt wzrokowy z przewodnikiem, a nie fiksacja na bodźcu." 
-        }
-    ],
-}
+export const TRAINING_ARTICLES = [
+    { id: "train_tree", title: "Metoda Drzewa (Luźna smycz)", keywords: ["smycz", "ciągnięcie", "spacer", "napięcie", "ciągnie"], desc: "Podstawowy trening spacerowy. Gdy tylko poczujesz, że smycz napina się choćby minimalnie, natychmiast zatrzymaj się i stój nieruchomo jak drzewo. Nie szarp psa, nie krzycz. Rusz przed siebie DOPIERO wtedy, gdy pies sam poluzuje smycz lub odwróci się i spojrzy na Ciebie. Uczy to psa, że tylko luźna smycz pozwala iść do przodu." },
+    { id: "train_asphalt", title: "Reguła 5 Sekund (Ochrona łap w lato)", keywords: ["lato", "asfalt", "upał", "poparzenie", "poduszki", "chodnik"], desc: "Zanim wyjdziesz z psem na spacer na rozgrzany chodnik w słoneczny dzień, przyłóż wewnętrzną część swojej dłoni płasko do asfaltu i przytrzymaj przez 5 sekund. Jeśli podłoże parzy Ciebie, oznacza to, że poparzy również delikatne opuszki Twojego psa. W upały spaceruj wyłącznie rano, wieczorem lub po trawiastych terenach leśnych." },
+    { id: "train_bingo", title: "Przywołanie Awaryjne (Słowo Życia)", keywords: ["ucieczka", "przywołanie", "bingo", "zagrożenie", "wróć", "wołąnie"], desc: "Wybierz jedno unikalne, dźwięczne słowo, którego NIGDY nie używasz na co dzień (np. 'BINGO!', 'BANAN!'). Przez kilka tygodni ćwicz w domu: wypowiedz słowo i natychmiast daj psu najlepszy smakołyk na świecie (np. kawałek pieczonego mięsa). To słowo ma kojarzyć się z absolutnym rajem. Używaj lo w terenie TYLKO wtedy, gdy pies np. biegnie w stronę ulicy. Nigdy nie używaj go na darmo." },
+    { id: "train_rule3", title: "Reguła 3 Sekund (Głaskanie obcych psów)", keywords: ["głaskanie", "dotyk", "obcy pies", "dzieci", "kontakt", "gryzie"], desc: "Zanim dotkniesz jakiegokolwiek psa, zapytaj właściciela o zgodę. Jeśli ją uzyskasz, kucnij bokiem do psa (nigdy nie podchodź od przodu i nie pochylaj się nad nim – to gest groźby). Wyciągnij delikatnie dłoń do powąchania. Pogłaskaj psa przez dokładnie 3 sekundy pod brodą lub po klatce piersiowej (NIGDY po czubku głowy!), po czym cofnij rękę. Jeśli pies odejdzie – zostaw go. Jeśli podejdzie bliżej i trąci Cię noskiem – oznacza to, że chce więcej." },
+    { id: "train_clicker", title: "Trening Klikerowy (Zaznaczanie zachowań)", keywords: ["kliker", "szkolenie", "nagroda", "smakołyk", "siad", "nauka"], desc: "Kliker to małe urządzenie wydające powtarzalny dźwięk 'klik'. Najpierw 'warunkujemy' kliker: klikasz i natychmiast dajesz smakołyk (powtórz 20 razy). Dźwięk staje się dla psa obietnicą nagrody. Następnie podczas nauki np. komendy 'siad', klikasz DOKŁADNIE w milisekundzie, gdy pupa psa dotyka ziemi, i dajesz nagrodę. Kliker pozwala precyzyjnie wskazać psu, za które zachowanie dostaje wypłatę." },
+    { id: "train_cage", title: "Klatka Kennelowa (Bezpieczny azyl)", keywords: ["klatka", "kennel", "lęk", "niszczenie", "samotność", "odpoczynek"], desc: "Klatka kennelowa nie jest więzieniem – to sztuczna jaskinia i azyl dla psa. Proces wprowadzania klatki musi być powolny: wrzucaj tam najlepsze gryzaki, podawaj miski z jedzeniem, nigdy nie zamykaj drzwiczek na siłę. Pies ma sam wybierać spanie w kennelówce. Prawidłowo wprowadzona klatka drastycznie obniża poziom stresu u psów z lękiem separacyjnym i zapobiega niszczeniu mieszkania." },
+    { id: "train_eye", title: "Nauka Skupienia (Kontakt Wzrokowy)", keywords: ["skupienie", "uwaga", "patrz", "kontakt", "smycz", "bodźce"], desc: "Podstawa pracy z psem reaktywnym. Weź smakołyk do dłoni, zamknij ją w pięść i wyciągnij w bok. Pies będzie lizał i drapał rękę. Stój nieruchomo. W momencie, gdy pies sfrustrowany brakiem dostępu do jedzenia odwróci wzrok od dłoni i spojrzy Ci prosto w oczy – powiedz 'TAK' i daj mu nagrodę. Uczy to psa, że kluczem do wszystkiego jest kontakt wzrokowy z przewodnikiem, a nie fiksacja na bodźcu." }
+];
