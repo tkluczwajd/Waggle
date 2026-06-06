71f31308c72ce3dfca0d0980f98c8c93d8ad6fb3
tkluczwajd <tkluczwajd@gmail.com>
2026-06-06 22:29:16 +0200

    Update wikiBreedData.js

--- src/data/wikiBreedData.js
+++ src/data/wikiBreedData.js
@@ -1,6 +1,3 @@
-// src/data/wikiData.js
 import { BREEDS_A_L } from './breedsAL.js';
 import { BREEDS_M_Z } from './breedsMZ.js';
-export const WIKI = {
- rasy: [...BREEDS_A_L, ...BREEDS_M_Z]
-};
+export const BREED_ARTICLES = [...BREEDS_A_L, ...BREEDS_M_Z];
