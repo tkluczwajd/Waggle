22a94e336a16f225b5ef6045ae0b24439d32b224
tkluczwajd <tkluczwajd@gmail.com>
2026-06-24 15:28:03 +0200

    Create index.js

---
+++ functions/index.js
@@ -0,0 +1,7 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+
+// Inicjalizacja środowiska administracyjnego Firebase
+admin.initializeApp();
+
+// Tutaj za chwilę dodamy funkcje wysyłające powiadomienia Push!
